5fecd3dbe233758d5bd635388d796f82a1171771
Artur Shalaev <imshaart@gmail.com>
2019-08-25 19:49:37 +0300

    JavaFX initialization returned back to test

--- src/test/java/shaart/pstorage/SpringAbstractTest.java
+++ src/test/java/shaart/pstorage/SpringAbstractTest.java
@@ -1,5 +1,7 @@
 package shaart.pstorage;
 
+import javafx.embed.swing.JFXPanel;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,4 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public abstract class SpringAbstractTest {
 
+  @BeforeClass
+  public static void bootstrapJavaFx() {
+    new JFXPanel();
+  }
 }
