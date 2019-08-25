3071a5d229f9e400a8906c0c257bc8cf45a41a13
Artur Shalaev <imshaart@gmail.com>
2019-08-25 18:36:37 +0300

    JavaFX initialization removed from tests

--- src/test/java/shaart/pstorage/SpringAbstractTest.java
+++ src/test/java/shaart/pstorage/SpringAbstractTest.java
@@ -1,7 +1,5 @@
 package shaart.pstorage;
 
-import javafx.embed.swing.JFXPanel;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,8 +8,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public abstract class SpringAbstractTest {
 
-  @BeforeClass
-  public static void bootstrapJavaFx() {
-    new JFXPanel();
-  }
 }
