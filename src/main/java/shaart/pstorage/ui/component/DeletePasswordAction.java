1d7a559f9413c6c14e68e7985d7d8ba479c00181
Artur Shalaev <imshaart@gmail.com>
2020-03-07 18:39:31 +0300

    Password removing implemented (#19)

--- src/main/java/shaart/pstorage/ui/component/DeletePasswordAction.java
+++ src/main/java/shaart/pstorage/ui/component/DeletePasswordAction.java
@@ -1,7 +1,7 @@
 package shaart.pstorage.ui.component;
 
 import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.Consumer;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
@@ -12,7 +12,7 @@ import shaart.pstorage.dto.PasswordDto;
 import shaart.pstorage.ui.CommandFactory;
 
 @Slf4j
-public class CopyPasswordAction {
+public class DeletePasswordAction {
 
   public static final CommandFactory COMMAND_FACTORY = CommandFactory.INSTANCE;
 
@@ -20,28 +20,26 @@ public class CopyPasswordAction {
    * Creates a callback that applies decrypt function and performs 'copy to clipboard' action on
    * resulting value.
    *
-   * @param decryptPasswordFunction function to extract decrypted value from {@link PasswordDto}
+   * @param removePassword function to delete password using {@link PasswordDto}
    * @return resulting callback for table column factory
    */
   public static Callback<
       TableColumn<PasswordDto, Button>,
       TableCell<PasswordDto, Button>
-      > createCallback(Function<PasswordDto, String> decryptPasswordFunction) {
-    return ActionButtonTableCell.forTableColumn("Copy to clipboard",
-        getProcessEventFunction(decryptPasswordFunction));
+      > createCallback(Consumer<PasswordDto> removePassword) {
+    return ActionButtonTableCell.forTableColumn("Delete",
+        getProcessEventFunction(removePassword));
   }
 
   private static BiFunction<ActionEvent, PasswordDto, PasswordDto> getProcessEventFunction(
-      Function<PasswordDto, String> decryptPasswordFunction) {
+      Consumer<PasswordDto> removePassword) {
     return (event, passwordDto) -> {
       try {
-        log.info("Got 'copy to clipboard' action for password with alias '{}'",
+        log.info("Got 'remove password' action for password with alias '{}'",
             passwordDto.getAlias());
-        final String decrypted = decryptPasswordFunction.apply(passwordDto);
-        final Runnable copyPasswordAction = COMMAND_FACTORY.createCopyPasswordAction(decrypted);
-        copyPasswordAction.run();
+        removePassword.accept(passwordDto);
 
-        log.info("'copy to clipboard' action for password with alias '{}' performed",
+        log.info("'remove password' action for password with alias '{}' performed",
             passwordDto.getAlias());
       } catch (Exception e) {
         log.error(e.getMessage(), e);
